a4d5ef3719a9d35c20d479d42f673bc6b569060e
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-22 18:03:54 +0530

    Added: The Random check has been used in case.

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -10,3 +10,23 @@ position=0
 
 #CHECKS THE NUMBER BETWEEN 1-6
 randomRoll=$((RANDOM%6+1))
+randomOption=$((RANDOM%3))
+NOPLAY=0
+LADDER=1
+SNAKE=2
+
+#SWITCH CASE FOR RANDOM OPTION
+case $randomOption in
+	$NOPLAY)
+		position=$position
+		echo "No play for the user"
+	;;
+	$LADDER)
+		position=$(($positon+$randomRoll))
+		echo "Position of a user is $position"
+	;;
+	$SNAKE)
+		position=$(($position-$randomRoll))
+		echo "Position after Bitting is $position"
+	;;
+esac
