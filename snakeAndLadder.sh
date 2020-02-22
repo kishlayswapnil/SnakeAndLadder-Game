0d601e9c9aadd0b22a15cb2ca6cb0ada54ce2c5c
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-22 18:11:48 +0530

    Added: While loop has been added to check upto 100 position.

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -7,26 +7,32 @@ WINPOSITION=100
 
 #Varriables Declared.
 position=0
+while [[ $position -ne $WINPOSITION ]]
+do
+	#CHECKS THE NUMBER BETWEEN 1-6
+	randomRoll=$((RANDOM%6+1))
+	randomOption=$((RANDOM%3))
+	NOPLAY=0
+	LADDER=1
+	SNAKE=2
 
-#CHECKS THE NUMBER BETWEEN 1-6
-randomRoll=$((RANDOM%6+1))
-randomOption=$((RANDOM%3))
-NOPLAY=0
-LADDER=1
-SNAKE=2
-
-#SWITCH CASE FOR RANDOM OPTION
-case $randomOption in
-	$NOPLAY)
-		position=$position
-		echo "No play for the user"
-	;;
-	$LADDER)
-		position=$(($positon+$randomRoll))
-		echo "Position of a user is $position"
-	;;
-	$SNAKE)
-		position=$(($position-$randomRoll))
-		echo "Position after Bitting is $position"
-	;;
-esac
+	#SWITCH CASE FOR RANDOM OPTION
+	case $randomOption in
+		$NOPLAY)
+			position=$position
+			echo "No play for the user"
+		;;
+		$LADDER)
+			position=$(($positon+$randomRoll))
+			echo "Position of a user is $position"
+		;;
+		$SNAKE)
+			position=$(($position-$randomRoll))
+			echo "Position after Bitting is $position"
+			if [ $position -lt $STARTPOSITION ]
+			then
+				position=$STARTPOSITION
+			fi
+		;;
+	esac
+done
